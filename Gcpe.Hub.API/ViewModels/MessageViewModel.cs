30f2e08dbd0063227e9d5b0fca54c0363ee0aca6
Brian Richter <brianorwhatever@gmail.com>
2018-11-27 12:19:37 -0800

    Messages controller and unit tests

---
+++ Gcpe.Hub.API/ViewModels/MessageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gcpe.Hub.API.ViewModels
+{
+    public class MessageViewModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int SortOrder { get; set; }
+        public bool IsHighlighted { get; set; }
+        public bool IsPublished { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
